854f29c099d71b25afe11c50afb2caf2d7935f27
seunggu <imaster0209@naver.com>
2019-06-27 01:46:16 +0900

    chore: init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+require('dotenv-safe').config();
+
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './App.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(8000);
+}
+
+bootstrap();
