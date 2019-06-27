73f10ce78ef0724f8b8fdf9b906e45b1680bd283
seunggu <imaster0209@naver.com>
2019-06-27 12:32:05 +0900

    fix: user repositoy interface

--- src/user/domain/user/UserRepository.interface.ts
+++ src/user/domain/user/UserRepository.interface.ts
@@ -4,7 +4,7 @@ import { User } from './User';
 export const USER_REPOSITORY_TOKEN = 'USER_REPOSITORY_TOKEN';
 
 export interface IUserRepository {
-  findById(userId: UserId): Promise<User>;
+  findById(userId: UserId): Promise<User|null>;
   save(user: User): Promise<void>;
   remove(user: User): Promise<void>;
 }
